82aa0331733a16dffa2b983083ff2011813df7cc
sudodeepak <sudodeepak@gmail.com>
2022-08-13 12:24:39 +0530

    #8

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
@@ -11,7 +9,6 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-import NextNProgress from "nextjs-progressbar";
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -24,13 +21,6 @@ export default function App({ Component, pageProps }) {
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
-      <NextNProgress
-         color="#ef4444"
-         startPosition={0.3}
-         stopDelayMs={200}
-         height={2}
-         showOnShallow={true}
-      />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
